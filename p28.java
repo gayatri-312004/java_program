5a8b8d53769f24bf5ccbafaf2b55d9d6380a5e6d
gayatri <gayatri31jan2004@gmail.com>
2025-06-10 23:14:57 +0530

    two files added

---
+++ p28.java
@@ -0,0 +1,12 @@
+import java.lang.System;
+import java.lang.String;
+
+class Demo{
+         public static void main(String[]args){
+            int chair1 = 1;
+            int chair2 = 2;
+         System.out.println(chair1);
+         System.out.println(chair2);
+      }
+
+}
